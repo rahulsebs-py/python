8ce753b32b9a78dcd4bea8dd8acb48823a9dff73
rahulsebs-py <rahulsebs3@gmail.com>
2026-06-12 18:01:31 +0530

    Create percentage calculator.py

---
+++ percentage calculator.py
@@ -0,0 +1,7 @@
+m1 = float(input("Enter mark 1: "))
+m2 = float(input("Enter mark 2: "))
+m3 = float(input("Enter mark 3: "))
+m4 = float(input("Enter mark 4: "))
+m5 = float(input("Enter mark 5: "))
+percentage = (m1 + m2 + m3 + m4 + m5) / 5
+print("Percentage =", percentage, "%")
