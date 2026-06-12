f364f9d979d70e5d6f2062d1155732aed1179d2e
rahulsebs-py <rahulsebs3@gmail.com>
2026-06-12 18:05:19 +0530

    Delete l calculator.py

--- p/l calculator.py
+++
@@ -1,8 +0,0 @@
-cost = float(input("Enter cost price: "))
-selling = float(input("Enter selling price: "))
-if selling > cost:
-    print("Profit =", selling - cost)
-elif cost > selling:
-    print("Loss =", cost - selling)
-else:
-    print("Draw match!")
